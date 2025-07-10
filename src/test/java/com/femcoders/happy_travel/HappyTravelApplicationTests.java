b4b5d4a89291e31483645b1afddbcc5257709cbf
VitaPoperechna <vitapoperechna@gmal.com>
2025-07-10 16:13:59 +0200

    first commit

---
+++ src/test/java/com/femcoders/happy_travel/HappyTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femcoders.happy_travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
