fdd98a222d9eb3a794eb2e17d2368fe3954f07b1
Alexandracoder <acasaviajes@gmail.com>
2025-07-11 12:40:03 +0200

    feat: add User model

---
+++ src/main/java/com/femcoders/happy_travel/models/User.java
@@ -0,0 +1,52 @@
+package com.femcoders.happy_travel.models;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles = new HashSet<>();
+
+    private boolean enabled = true;
+
+    private boolean accountNonLocked = true;
+
+    private boolean credentialsNonExpired = true;
+
+    private boolean accountNonExpired = true;
+
+}
