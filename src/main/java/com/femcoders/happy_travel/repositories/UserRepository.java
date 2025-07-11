021de984ac8e6face8eee095d0acf1709f3786d8
Alexandracoder <acasaviajes@gmail.com>
2025-07-11 12:42:29 +0200

    feat: add UserRepository interface

---
+++ src/main/java/com/femcoders/happy_travel/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.femcoders.happy_travel.repositories;
+
+import com.femcoders.happy_travel.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
+ 
