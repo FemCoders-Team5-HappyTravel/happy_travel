34e10c6ecf26de3aca2aba68e26491a7dc404f74
VitaPoperechna <vitapoperechna@gmal.com>
2025-07-14 14:22:35 +0200

    feat: added dto files in destination

--- src/main/java/com/femcoders/happy_travel/dtos/destination/DestinationRequest.java
+++ src/main/java/com/femcoders/happy_travel/dtos/destination/DestinationRequest.java
@@ -12,11 +12,11 @@ public record DestinationRequest(
         String country,
 
         @NotBlank(message = "City is required")
-        @Size(min = 2, max = 50, message = "Name must contain min 2 and max 50 characters")
+        @Size(min = 2, max = 50, message = "City must contain min 2 and max 50 characters")
         String city,
 
         @NotBlank(message = "Description is required")
-        @Size(min = 2, max = 300, message = "Name must contain min 2 and max 300 characters")
+        @Size(min = 2, max = 300, message = "Description must contain min 2 and max 300 characters")
         String description,
 
         @NotNull
