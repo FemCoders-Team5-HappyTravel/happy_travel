23d9c2dcfdd136571715bd35e17289438099d66b
May1704 <vergara.mayleris@gmail.com>
2025-07-30 11:03:23 +0200

    chore: add filter destination by location or title

--- src/main/java/com/femcoders/happy_travel/services/DestinationService.java
+++ src/main/java/com/femcoders/happy_travel/services/DestinationService.java
@@ -5,14 +5,13 @@ import java.util.List;
 
 public interface DestinationService {
     DestinationResponse createDestination(Long userId, DestinationRequest destinationRequest);
-    List<DestinationResponse> getAllDestinations();
     DestinationResponse getDestinationById(Long id);
     DestinationResponse updateDestination(Long id, DestinationRequest request);
     void deleteDestination(Long id);
     public boolean isUserOwner(Long userId, String username);
     public List<DestinationResponse> getDestinationsByUserId(Long userId);
     List<DestinationResponse> getDestinationsByUsername(String username);
-
+    List<DestinationResponse> getFilteredDestinations(String searchTerm);
 }
 
 
